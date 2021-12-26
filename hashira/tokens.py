4b8b52ae93d5462e57dd64c43eb28fecd5133287
Prabesh Shrestha <creatorworld329@gmail.com>
2021-12-26 21:56:58 +0545

    heroku default

--- hashira/tokens.py
+++ hashira/tokens.py
@@ -1,4 +1,4 @@
 # real
-# bot_token = 'ODM5ODMxNDQ4NjAwMTgyODQ0.YJPX_A.yYCiixb6lDxar3C5dRcnoogF0tA'
+bot_token = 'ODM5ODMxNDQ4NjAwMTgyODQ0.YJPX_A.yYCiixb6lDxar3C5dRcnoogF0tA'
 # test 
-bot_token = 'ODI1NTk0MDkyMTA5MDM3NTY5.YGAMag.Z5KlVRQ7L2qw3889gMRDLguXvRw'
+# bot_token = 'ODI1NTk0MDkyMTA5MDM3NTY5.YGAMag.Z5KlVRQ7L2qw3889gMRDLguXvRw'
