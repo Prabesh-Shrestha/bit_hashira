61194d992dcdb6011b45be842b07beca6bedbcd1
Prabesh Shrestha <creatorworld329@gmail.com>
2022-02-11 11:20:21 +0545

    heroku default

--- hashira/commands/miss.py
+++ hashira/commands/miss.py
@@ -1,12 +1,19 @@
 import lightbulb
 import hikari
+import hashira 
 mis = lightbulb.Plugin("mis")
 
 mis.description = f'''
+invite: 
+invite link of the bot
+
+{hashira.prefix}invite 
+
 ping: 
 shows the latency of the bot 
-ex: 
->ping
+
+{hashira.prefix}ping
+
 '''
 
 @mis.command()
@@ -17,6 +24,15 @@ async def ping(ctx: lightbulb.Context):
     embed.add_field(name='rn : ', value=f'{ctx.bot.heartbeat_latency * 1_000:,.0f}ms')
     embed.set_footer(icon=ctx.member.avatar_url, text=str(ctx.member))
     await ctx.respond(embed=embed)
+
+@mis.command()
+@lightbulb.command("invite", "invite link")
+@lightbulb.implements(lightbulb.PrefixCommand,lightbulb.SlashCommand)
+async def inv(ctx: lightbulb.Context):
+    embed = hikari.Embed()
+    embed.add_field(name='Link: ', value=hashira.INV_LINK)
+    await ctx.respond(embed=embed)
+
 
 
 
